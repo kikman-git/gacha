fedbc8c2eef76eab02d601a3b98b7a5d7768694c
daisuke <daisuke.zr@icloud.com>
2021-09-13 15:25:02 +0900

    add useraccout table

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,17 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.core.validators import EmailValidator
 # Create your models here.
 class UserAccountManager(BaseUserManager):
-    def create_user(self, username, store_password, password):
-        username = username
-        user = self.model(username=)
+    def create_user(self, username, password):
+        user = self.model(username=username, )
 
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, store_password, password):
-        user = self.create_user(email=email, store_password=store_password, password=password)
+    def create_superuser(self, username, password):
+        user = self.create_user(username=username, password=password)
         user.is_superuser = True
         user.save()
 
@@ -22,6 +21,9 @@ class UserAccountManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    gaccha_chances = models.IntegerField(null=True)
+    rakuten_point = models.IntegerField(null=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -32,17 +34,19 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
 
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
     #REQUIRED_FIELDS is essntial text besides USENAME_FIELD and password
-    REQUIRED_FIELDS = ['store']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
 
 
-    def __get_label(self, field):
-        return text_type(self._meta.get_field(field).verbose_name)
+    # def __get_label(self, field):
+    #     return text_type(self._meta.get_field(field).verbose_name)
 
-    @property
-    def email_label(self):
-        return self.__get_label('email')
+    # @property
+    # def email_label(self):
+    #     return self.__get_label('email')
+
+
