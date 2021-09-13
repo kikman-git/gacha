fc3099b708ce304047d714052efe6a72588d793f
Aiyovei-ting <52597330+Aiyovei-ting@users.noreply.github.com>
2021-09-14 08:35:20 +0900

    delete node_module add .gitignore add .editorconfig rename homepage.js to Homepage.js

---
+++ gacha/src/components/Homepage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/Homepage.css';
+import logoLink from '../resources/rakuten_logo.svg';
+import Footer from './components/footer';
+
+function Homepage() {
+
+  function Login() {
+    console.log('Login function');
+  }
+  function Register() {
+    console.log('Register function');
+  }
+  return (
+    <div className="homepage">
+
+      <img src={logoLink}
+        alt="logo"
+        className="Logo" />
+      <p className="gameRule">some game rules</p>
+      <button onClick={Login}>login</button>
+      <button onClick={Register}>Register</button>
+      <Footer />
+    </div>
+  );
+}
+
+export default Homepage;
