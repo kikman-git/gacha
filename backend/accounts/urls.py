1cf962c9e497216835d710fa95cc91f2616ee019
daisuke <daisuke.zr@icloud.com>
2021-09-14 13:57:46 +0900

    customize login api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-from .views import RegisterView
+from .views import RegisterView, CustomAuthToken
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('login/', CustomAuthToken.as_view())
 ]
--- backend/coupons/views.py
+++ backend/coupons/views.py
@@ -1,15 +1,15 @@
 from coupons.models import Coupons
 from coupons.serializers import CouponSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class CouponList(generics.ListCreateAPIView):
     queryset = Coupons.objects.all()
     serializer_class = CouponSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class CouponDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Coupons.objects.all()
     serializer_class = CouponSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
