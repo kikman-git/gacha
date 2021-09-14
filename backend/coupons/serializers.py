1728dce8ebf86b3bb25a9656d48f7940b73aac22
daisuke <daisuke.zr@icloud.com>
2021-09-14 15:44:06 +0900

    add permission status

--- backend/coupons/serializers.py
+++ backend/coupons/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import Coupons
 from rest_framework.validators import UniqueValidator
-
+from rest_framework.authtoken.models import Token
 
 class CouponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coupons
         fields = ['id', 'value', 'user', 'genre', 'status', 'expire_date']
+
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        token =request.auth.key
+        user = Token.objects.get(key=token).user
+        if validated_data["user"].id == user.id:
+            return Coupons.objects.create(**validated_data)
+        else:
+            validated_data["user"] = user
+            return Coupons.objects.create(**validated_data)
--- backend/coupons/views.py
+++ backend/coupons/views.py
@@ -1,15 +1,51 @@
+from django.http.response import JsonResponse
 from coupons.models import Coupons
 from coupons.serializers import CouponSerializer
 from rest_framework import generics
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from .permission import AuthenticatedUserOnly
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.core.exceptions import PermissionDenied
+import json
 
-class CouponList(generics.ListCreateAPIView):
+class CouponCreate(generics.CreateAPIView):
     queryset = Coupons.objects.all()
     serializer_class = CouponSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AuthenticatedUserOnly,)
+
+
+    def valid_request(self, request):
+        body_unicode = request.body.decode('utf-8')
+        body_data = json.loads(body_unicode)
+        token =self.request.auth.key
+        user = Token.objects.get(key=token).user
+        if body_data['user'] == str(user.id):
+            return True
+        else:
+            PermissionDenied()
+
+
+
+class CouponList(generics.ListAPIView):
+    queryset = Coupons.objects.all()
+    serializer_class = CouponSerializer
+    permission_classes = (AuthenticatedUserOnly,)
+
+    def get(self, request):
+        try:
+            token =self.request.auth.key
+            user = Token.objects.get(key=token).user
+            coupons =  Coupons.objects.filter(user=user)
+            serializer = CouponSerializer(coupons, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            print(e)
+            raise PermissionDenied()
 
 
 class CouponDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Coupons.objects.all()
     serializer_class = CouponSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AuthenticatedUserOnly,)
