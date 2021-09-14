4eea18be50ae5c825d66a76bbae52c36b6368674
Phat Nguyen <kikman.star@gmail.com>
2021-09-14 16:25:52 +0900

    install axios

--- gacha/src/components/Loginpage.js
+++ gacha/src/components/Loginpage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../styles/Login.css';
 
 export default class Loginpage extends Component {
-
   constructor(props) {
     super(props);
     this.Login = this.Login.bind(this);
@@ -18,7 +17,8 @@ export default class Loginpage extends Component {
         <p>
           user ID: <input type="text" name="userID" maxlength="20"></input>
           <br></br>
-          password: <input type="password" name="password" maxlength="16"></input>
+          password:{' '}
+          <input type="password" name="password" maxlength="16"></input>
         </p>
         <p>
           <button onClick={this.Login}>login</button>
