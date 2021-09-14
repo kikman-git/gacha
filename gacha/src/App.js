f4d48e056d155dac3294e11757eddb945bdd4697
Xinrui Fang <xinrui.fang@keio.jp>
2021-09-14 15:47:46 +0900

    init

--- gacha/src/App.js
+++ gacha/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Gacha from './components/Gacha';
+import Award from './components/Award';
 
 function App() {
   return (
     <div className="App">
-       <Gacha />
+      <Router>
+        <Route exact path="/" component={Gacha}  />
+        <Route path="/Award" component={Award}  />
+      </Router>
     </div>
   );
 }
---
+++ gacha/src/components/Coupon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Coupon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>
+                <h1>This is Award Page</h1>
+            </div>
+            
+        );
+    }
+}
+
+export default Coupon;
