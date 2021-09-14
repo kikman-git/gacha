4d877399d59631b1c5190816eb73176281a97145
xin <xizig2018@gmail.com>
2021-09-14 14:07:37 +0900

    modify GetAdditionalChance function to class add LackofChance page

--- gacha/src/components/GetAdditionChance.js
+++ gacha/src/components/GetAdditionChance.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/GetAdditionChance.css';
 
-// import {Container, Wrapper, Row, Column, Link, Title} from './footer';
+export default class GetAdditionChance extends Component {
 
-function GetAdditionalChance() {
-  return (
-    <div className="App">
+  constructor(props) {
+    super(props);
+    // this.Login = this.Login.bind(this);
+    // this.Register = this.Register.bind(this);
+  }
+
+  // Login() {
+  //   this.props.history.push('/login');
+  // }
+
+  // Register() {
+  //   this.props.history.push('/register');
+  // }
+
+  render() {
+    return (
+      <div className="getadditionchance">
         <h1>GetAdditionalChance</h1>
         <a href="" className='btn btn--orange btn--radius'>Get a chance for every 5000yen spent in rakutenichiba</a>
         <br></br>
@@ -17,9 +31,7 @@ function GetAdditionalChance() {
         <br></br>
         <br></br>
         <br></br>
-      
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-export default GetAdditionalChance;
