7d4bbc2e0d6fcd86c6ac2ebd0ef9618584f8846b
xin <xizig2018@gmail.com>
2021-09-14 12:00:44 +0900

    add GetAdditionalChance page

---
+++ gacha/src/components/GetAdditionChance.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/GetAdditionChance.css';
+
+// import {Container, Wrapper, Row, Column, Link, Title} from './footer';
+
+function GetAdditionalChance() {
+  return (
+    <div className="App">
+        <h1>GetAdditionalChance</h1>
+        <a href="" className='btn btn--orange btn--radius'>Get a chance for every 5000yen spent in rakutenichiba</a>
+        <br></br>
+        <br></br>
+        <a href="" className='btn btn--orange btn--radius'>Use rakuten points to buy  chance </a>
+        <br></br>
+        <br></br>
+        <a href="" className='btn btn--orange btn--radius'>share with one friend to get a free GACHA</a>
+        <br></br>
+        <br></br>
+        <br></br>
+      
+    </div>
+  );
+}
+
+export default GetAdditionalChance;
