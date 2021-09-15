53eb13de576892e6bfb3d901dbae5e1bb9010a0b
cloudest-y <cloudestLiu@users.noreply.github.com>
2021-09-15 16:37:06 +0900

    header
    with Rakuten Logo

--- gacha/src/components/header.js
+++ gacha/src/components/header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import '../styles/Header.css';
+import logo from '../resources/RakutenLogo.png';
+
 
 
 function Header() {
 
     return (
         <div className="header">
-            <h3>Let's Gacha!</h3>
+          <div className="logo">
+            <a herf="https://www.rakuten.co.jp/">
+                <img src={logo} width="175" height="100" alt="logo" /></a>
+          </div><nav>
+                <ul>
+                    <h3>Let's Gacha!</h3>
+                </ul>
+            </nav>
         </div>
+        // <div className="header">
+        //     {/* <div className="logo">
+        //         <a href="https://www.rakuten.co.jp/">
+        //             <img src={logo} alt = "logo"/>
+        //         </a>
+        //     </div> */}
+        //     <h3>Let's Gacha!</h3>
+        // </div>
     );
   }
   
