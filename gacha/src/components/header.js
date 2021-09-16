459368d18386a3b49391a7499de7a64c0d177abc
Aiyovei-ting <52597330+Aiyovei-ting@users.noreply.github.com>
2021-09-16 10:17:23 +0900

    change header

--- gacha/src/components/header.js
+++ gacha/src/components/header.js
@@ -6,28 +6,27 @@ import logo from '../resources/RakutenLogo.png';
 
 function Header() {
 
-    return (
-        <div className="header">
-          <div className="logo">
-            <a herf="https://www.rakuten.co.jp/">
-                <img src={logo} width="175" height="100" alt="logo" /></a>
-          </div><nav>
-                <ul>
-                    <h3>Let's Gacha!</h3>
-                </ul>
-            </nav>
-        </div>
-        // <div className="header">
-        //     {/* <div className="logo">
-        //         <a href="https://www.rakuten.co.jp/">
-        //             <img src={logo} alt = "logo"/>
-        //         </a>
-        //     </div> */}
-        //     <h3>Let's Gacha!</h3>
-        // </div>
-    );
-  }
-  
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo">
+        <a herf="#">
+          <img src={logo} width="175" height="100" alt="logo" /></a>
+      </div>
+      {/* <nav>
+        <ul>
+          <h3>Let's Gacha!</h3>
+        </ul>
+      </nav> */}
+    </div>
+    // <div className="header">
+    //     {/* <div className="logo">
+    //         <a href="https://www.rakuten.co.jp/">
+    //             <img src={logo} alt = "logo"/>
+    //         </a>
+    //     </div> */}
+    //     <h3>Let's Gacha!</h3>
+    // </div>
+  );
+}
 
- 
+export default Header;
